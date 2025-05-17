180c24237ccebcb6b5b5f5c482b250cb8cafb19a
sanjai <sanjaiposeidon@gmail.com>
2025-05-17 10:17:05 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,10 @@
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  // std::cout << "$ ";
-
+ 
   std::string input;
   std::getline(std::cin, input);
 }
