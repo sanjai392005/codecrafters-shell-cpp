255c3ecddaf8d2c4863828678e8b1ea33a6d819c
sanjai <sanjaiposeidon@gmail.com>
2025-05-17 11:50:47 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <string>
+#include <set>
 using namespace std;
 
 int main(){
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
+
+    set<string> shell_commands = {"type", "echo"};
 
     while(true){
         string command;
@@ -13,14 +16,22 @@ int main(){
 
         if(command=="exit 0" || command=="exit"){
             return 0;
-        }else if(command.substr(0, 4)=="echo"){
+        }
+        else if(command.substr(0, 4)=="echo"){
+
             if(command.size()==4) continue;
             else cout<<command.substr(5, command.size())<<endl;
-        }else if(command.substr(0, 4)=="type"){
-            if(command.size()==4){ continue; }
-            else cout<<command.substr(5, command.size())<<" is a shell builtin"<<endl;
+
         }
-        
+        else if(command.substr(0, 4)=="type"){
+
+            string suffix = command.substr(5, command.size());
+
+            if(command.size()==4){ continue; }
+            else if(shell_commands.find(suffix)!=shell_commands.end()) cout<<suffix<<" is a shell builtin"<<endl;
+            else if(shell_commands.find(suffix)==shell_commands.end()) cout<<suffix<<": command not found" << endl;
+            
+        }
         else{
             cout<<command<<": command not found" << endl;
         }
