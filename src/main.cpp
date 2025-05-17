3793ba1152302d7e2b59108688e7db6ff745b6d2
sanjai <sanjaiposeidon@gmail.com>
2025-05-17 11:33:35 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,17 @@ int main(){
         cout<<"$ ";
         getline(cin, command);
 
-        if(command=="exit 0"){
+        if(command=="exit 0" || command=="exit"){
             return 0;
         }else if(command.substr(0, 4)=="echo"){
-            cout<<command.substr(5, command.size())<<endl;
-        }else{
+            if(command.size()==4) continue;
+            else cout<<command.substr(5, command.size())<<endl;
+        }else if(command.substr(0, 4)=="type"){
+            if(command.size()==4){ continue; }
+            else cout<<command.substr(5, command.size())<<" is a shell builtin"<<endl;
+        }
+        
+        else{
             cout<<command<<": command not found" << endl;
         }
     }
