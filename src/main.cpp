ccdae89600ad7695484c286a58876b4f75d93e8d
sanjai <sanjaiposeidon@gmail.com>
2025-05-17 15:44:14 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
+#include <cstdlib>
 #include <string>
 #include <set>
+#include <vector>
+#include <filesystem>
 using namespace std;
+namespace fs = std::filesystem;
+
+vector<std::string> path_dealer(){ //splits the PATH at occurence of ':' and stores the individual paths in a vector
+    std::vector<std::string> paths;
+    std::string env_path = std::getenv("PATH");
+
+    std::string seperate_path = "";
+
+    for(char i: env_path){
+        if(i!=':'){
+           seperate_path.push_back(i) ;
+        }
+        else{
+            paths.push_back(seperate_path);
+            seperate_path = "";
+        }
+    }
+    if(!seperate_path.empty()){
+        paths.push_back(seperate_path);
+    }
+
+    return paths;
+}
+
+std::string program_existence(std::vector<std::string> paths, std::string target){
+    int isPresent = 0;
+
+    for(std::string i: paths){
+        fs::path current_path = i;
+
+        for(const auto &entry: fs::directory_iterator(current_path)){
+            if(entry.path().filename() == target){
+                isPresent = 1;
+                return target+" is "+(std::string)entry.path()+"\n";
+            }
+        }
+    }
+
+    if(isPresent==0){
+        return target+": not found"+"\n";
+    }
+}
 
 int main(){
     std::cout << std::unitbuf;
@@ -14,27 +59,35 @@ int main(){
         cout<<"$ ";
         getline(cin, command);
 
-        if(command=="exit 0" || command=="exit"){
+        if(command=="exit 0" || command=="exit"){// INBUILT: exit
             return 0;
         }
-        else if(command.substr(0, 4)=="echo"){
+        else if(command.substr(0, 4)=="echo"){// INBUILT: echo
 
             if(command.size()==4){continue;}
             else cout<<command.substr(5, command.size())<<endl;
 
         }
-        else if(command.substr(0, 4)=="type"){
+        else if(command.substr(0, 4)=="type"){// INBUILT: type
             if(command.size()==4){ continue; }
             else{
                 string suffix = command.substr(5, command.size());
+
                 if(shell_commands.find(suffix)!=shell_commands.end()) cout<<suffix<<" is a shell builtin"<<endl;
-                else if(shell_commands.find(suffix)==shell_commands.end()) cout<<suffix<<": not found" << endl;
+                //else if(shell_commands.find(suffix)==shell_commands.end()) cout<<suffix<<": not found" << endl;
+                else{
+                    vector<string> paths = path_dealer();
+                    cout<<program_existence(paths, suffix);
+                }
+
             }
 
         }
-        else{
-            cout<<command<<": command not found" << endl;
-        }
+
+        // else{
+        //     cout<<command<<": command not found" << endl;
+        // }
     }
 
+    return 0;
 }
