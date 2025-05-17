7ff958d1a65b3da86dd4739e754bb49b35e5e8c8
sanjai <sanjaiposeidon@gmail.com>
2025-05-17 10:49:30 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 int main(){
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-  std::cout << "$ ";
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
+    std::cout << "$ ";
 
     string input;
     cin>>input;
     cout<<input << ": command not found" << endl;
+    main();
 }
